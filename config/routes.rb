7ba74934e14bd43a63e55745345f4bd61ae622a4
usaginoshippo <86910445+usaginoshippo@users.noreply.github.com>
2021-11-06 17:16:59 +0900

    プロトタイプの詳細表示の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
   root to: 'prototypes#index'
-  resources :prototypes,  only: [:index, :new, :create]
+  resources :prototypes,  only: [:index, :new, :create, :show]
 
 end
