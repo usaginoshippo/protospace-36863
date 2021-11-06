8e1759ffadce1fd8e636525748930c83c9f73ef5
usaginoshippo <86910445+usaginoshippo@users.noreply.github.com>
2021-11-06 18:30:29 +0900

    削除機能の実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -33,6 +33,11 @@ class PrototypesController < ApplicationController
       render :edit
   end
 
+  def destroy
+    prototype =Prototype.find(params[:id])
+    prototype.destroy
+    redirect_to root_path
+  end
   private
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
